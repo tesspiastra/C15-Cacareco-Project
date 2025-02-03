56a52bb3ec8260cb3b453262e04306eb7e898832
CodeTechBen <benjamin-smith@live.com>
2025-02-03 17:35:54 +0000

    best shell script in the world

--- schema/connect.sh
+++ schema/connect.sh
@@ -1,8 +1,16 @@
 source ../pipeline/.env
 
-if [ "$1" == "reset" ]; then
-    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -i "path/to/your/schema.sql"
+if [ "_$1_" == "_reset_" ]; then
+    # Set default schema for the user
+    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -Q "ALTER USER [$DB_USER] WITH DEFAULT_SCHEMA = [$SCHEMA_NAME]"
+    
+    # Run schema.sql to reset the database schema
+    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -i "schema.sql"
+    
     echo "Database schema has been reset."
 else
-    sqlcmd -S $DB_HOST,$DB_PORT -U $DB_USER -P $DB_PASSWORD -d $DB_NAME
+    # Set default schema for the user
+    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -Q "ALTER USER [$DB_USER] WITH DEFAULT_SCHEMA = [$SCHEMA_NAME]"
+
+    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME"
 fi
