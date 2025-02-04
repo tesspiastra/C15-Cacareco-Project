2fc8e0d6ae6d9a0df5dd57b1f87edb12deaa2b95
tesspiastra <tpiastra246@gmail.com>
2025-02-04 11:53:21 +0000

    terraform files for lambda, names not changes

---
+++ pipeline/terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "DB_HOST" {
+  type = string
+}
+
+variable "DB_NAME" {
+  type = string
+}
+
+variable "DB_PASSWORD" {
+  type = string
+}
+
+variable "DB_USER" {
+  type = string
+}
+
+variable "DB_PORT" {
+}
+
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
