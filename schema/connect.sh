dac48ec4264e686ad6eaa9ff2e1f88c055d96007
CodeTechBen <benjamin-smith@live.com>
2025-02-04 13:01:36 +0000

    connect script can now seed db

--- schema/connect.sh
+++ schema/connect.sh
@@ -1,16 +1,20 @@
+# Run using bash connect.sh [seed|reset]
+
 source ../pipeline/.env
 
+# Set default schema for the user
+sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -Q "ALTER USER [$DB_USER] WITH DEFAULT_SCHEMA = [$SCHEMA_NAME]"
+
 if [ "_$1_" == "_reset_" ]; then
-    # Set default schema for the user
-    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -Q "ALTER USER [$DB_USER] WITH DEFAULT_SCHEMA = [$SCHEMA_NAME]"
-    
     # Run schema.sql to reset the database schema
     sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -i "schema.sql"
-    
     echo "Database schema has been reset."
-else
-    # Set default schema for the user
-    sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME" -Q "ALTER USER [$DB_USER] WITH DEFAULT_SCHEMA = [$SCHEMA_NAME]"
 
+elif [ "_$1_" == "_seed_" ]; then
+    # Run the seed_master_data.py
+    python3 seed_master_data.py
+
+else
+    # Connect to the DB in terminal
     sqlcmd -S "$DB_HOST,$DB_PORT" -U "$DB_USER" -P "$DB_PASSWORD" -d "$DB_NAME"
 fi
