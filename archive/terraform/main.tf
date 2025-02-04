6fd2d03296b1ee249056247002f8f16c6bfeef76
AbdulDahir7002 <abdul.dahir7002@gmail.com>
2025-02-04 16:38:28 +0000

    archive lambda terraform

--- archive/terraform/main.tf
+++ archive/terraform/main.tf
@@ -48,3 +48,85 @@ resource "aws_iam_user_group_membership" "user_group_membership" {
   user   = each.value
   groups = [aws_iam_group.s3_access_group.name]
 }
+
+# Lambda build
+
+data "aws_ecr_repository" "lambda-repo" {
+  name = "c15-cacareco-lmnh-plants-archive-lambda"
+}
+
+data "aws_ecr_image" "lambda-image" {
+  repository_name = data.aws_ecr_repository.lambda-repo.name
+  image_tag = "latest"
+}
+
+# Trust doc
+data "aws_iam_policy_document" "lambda-role-trust-policy-doc" {
+    statement {
+      effect = "Allow"
+      principals {
+        type = "Service"
+        identifiers = [ "lambda.amazonaws.com" ]
+      }
+      actions = [
+        "sts:AssumeRole"
+      ]
+    }
+}
+
+# Permissions doc
+data "aws_iam_policy_document" "lambda-role-permissions-policy-doc" {
+    statement {
+      effect = "Allow"
+      actions = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      resources = [ "arn:aws:logs:eu-west-2:129033205317:*" ]
+    }
+}
+
+# Role
+resource "aws_iam_role" "lambda-role" {
+    name = "c15-cacareco-archive-role"
+    assume_role_policy = data.aws_iam_policy_document.lambda-role-trust-policy-doc.json
+}
+
+# Permissions policy
+resource "aws_iam_policy" "lambda-role-permissions-policy" {
+    name = "c15-cacareco-lambda-permissions-policy"
+    policy = data.aws_iam_policy_document.lambda-role-permissions-policy-doc.json
+}
+
+# Connect the policy to the role
+resource "aws_iam_role_policy_attachment" "lambda-role-policy-connection" {
+  role = aws_iam_role.lambda-role.name
+  policy_arn = aws_iam_policy.lambda-role-permissions-policy.arn
+}
+
+resource "aws_lambda_function" "lambda-report" {
+  function_name = "c15-cacareco-archive-lambda"
+  timeout = 10
+  image_uri = data.aws_ecr_image.lambda-image.image_uri
+  package_type = "Image"
+
+  memory_size = 512
+  ephemeral_storage {
+    size = 512
+  }
+
+  role = aws_iam_role.lambda-role.arn
+
+  environment {
+    variables = {
+    DB_HOST=var.DB_HOST
+    DB_PORT=var.DB_PORT
+    DB_NAME=var.DB_NAME
+    DB_USER=var.DB_USER
+    DB_PASSWORD=var.DB_PASSWORD
+    SCHEMA_NAME=var.SCHEMA_NAME
+    }
+  }
+  
+}
--- archive/terraform/variables.tf
+++ archive/terraform/variables.tf
@@ -9,3 +9,27 @@ variable "AWS_SECRET_ACCESS_KEY" {
 variable "REGION" {
   type = string
 }
+
+variable "DB_HOST" {
+  type = string
+}
+
+variable "DB_NAME" {
+  type = string
+}
+
+variable "DB_PASSWORD" {
+  type = string
+}
+
+variable "DB_USER" {
+  type = string
+}
+
+variable "DB_PORT" {
+  type = string
+}
+
+variable "SCHEMA_NAME" {
+  type = string
+}
